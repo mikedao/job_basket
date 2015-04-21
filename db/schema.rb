818c7d995c10ad472ddea79f27c18fa17108aea4
Emily Berkeley <eberkeley@gmail.com>
2015-04-21 14:47:57 -0600

    Add company model

---
+++ db/schema.rb
@@ -0,0 +1,69 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20150421204518) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "hiddenjobs", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "job_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "hiddenjobs", ["job_id"], name: "index_hiddenjobs_on_job_id", using: :btree
+  add_index "hiddenjobs", ["user_id"], name: "index_hiddenjobs_on_user_id", using: :btree
+
+  create_table "jobs", force: :cascade do |t|
+    t.string   "position"
+    t.date     "posting_date"
+    t.text     "description"
+    t.string   "location"
+    t.integer  "company_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  create_table "jobtags", force: :cascade do |t|
+    t.integer  "tag_id"
+    t.integer  "job_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "jobtags", ["job_id"], name: "index_jobtags_on_job_id", using: :btree
+  add_index "jobtags", ["tag_id"], name: "index_jobtags_on_tag_id", using: :btree
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
+    t.string   "token"
+    t.string   "uid"
+    t.string   "profile_image_url"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+  end
+
+  add_foreign_key "hiddenjobs", "jobs"
+  add_foreign_key "hiddenjobs", "users"
+  add_foreign_key "jobtags", "jobs"
+  add_foreign_key "jobtags", "tags"
+end
