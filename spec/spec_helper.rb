53ca492c9cc950ab1fa195f37e804015ca1b3ced
Michael Dao <michael.dao@gmail.com>
2015-04-21 09:51:09 -0600

    Made hound fixes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,6 @@ require "rails_helper"
 require "capybara/rspec"
 
 RSpec.configure do |config|
-
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
