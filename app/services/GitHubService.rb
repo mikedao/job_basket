54bcb143beb9082395214bcfcbd9d366b7831c09
Adam Smith <adamsmith910@aol.com>
2015-04-22 09:11:28 -0600

    set up githubservice file

--- app/services/GitHubService.rb
+++ app/services/GitHubService.rb
@@ -1,2 +1,27 @@
 class GitHubService
+  def initialize
+    @connection = Faraday.new(url: "https://jobs.github.com/positions.json?")
+  end
+
+  def save_jobs_data
+    data = parse(get_data(langague, location))
+    JobsDataGenerator.call(data)
+  end
+
+  private
+
+  def get_data(language, location)
+    @connection.get do |req|
+      req.url "description=#{language}&location=#{location}"
+      req.params["get"] = generate_tables(GitHubDataMapper.table_names(language), language)
+    end
+  end
+
+  def generate_tables(table_number, specifer=nil)
+    GitHubTableQuery.format(table_number, specifer)
+  end
+
+  def parse(data)
+    JSON.parse(data.body)
+  end
 end
