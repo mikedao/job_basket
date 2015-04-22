a4862c32d4540304729bc19ba7d7560dfd4c527d
Nathan Owsiany <nowsiany@gmail.com>
2015-04-21 18:39:03 -0600

    Fix hound issue

--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe Job, type: :model do
   describe "validations" do
-    it { should belong_to(:company)  }
+    it { should belong_to(:company) }
     it { should validate_presence_of(:position) }
     it { should validate_presence_of(:posting_date) }
     it { should validate_presence_of(:description) }
