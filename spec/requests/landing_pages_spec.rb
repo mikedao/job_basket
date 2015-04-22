bf11d1643d8fef10f83dcbde399b41931a12b6ac
Scott Crawford <scottcrawford03@gmail.com>
2015-04-22 15:29:22 -0600

    Update landing_pages_spec.rb

--- spec/requests/landing_pages_spec.rb
+++ spec/requests/landing_pages_spec.rb
@@ -3,11 +3,11 @@ require "rails_helper"
 RSpec.describe "LandingPages", type: :request do
   describe "GET /landing_pages" do
     before do
-      visit '/'
+      visit "/"
     end
 
     it "has job basket on the landing page" do
-      expect(page).to have_content('Job Basket')
+      expect(page).to have_content("Job Basket")
     end
   end
 end
