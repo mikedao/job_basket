a7504a0cd2b75099e877cab0b4672ca712cb0f1f
Michael Dao <michael.dao@gmail.com>
2015-04-22 14:35:11 -0600

    Added more company columns and tests

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -2,7 +2,29 @@ require "rails_helper"
 
 RSpec.describe Company, type: :model do
   describe "validations" do
-    it { should have_many(:jobs) }
-    it { should validate_presence_of(:name) }
+    it "should validate presence of name" do
+      company  = build(:company)
+      company2 = build(:company, name: nil)
+
+      expect(company).to be_valid
+      expect(company2).not_to be_valid
+    end
+
+    it "can have a job" do
+      company = create(:company)
+      company.jobs << create(:job)
+
+      expect(company.jobs.count).to eq(1)
+    end
+
+    it "can have many jobs" do
+      company = create(:company)
+      company.jobs << create(:job, position: "job1")
+      company.jobs << create(:job, position: "job2")
+
+      expect(company.jobs.count).to eq(2)
+      expect(company.jobs.first.position).to eq("job1")
+      expect(company.jobs.last.position).to eq("job2")
+    end
   end
 end
