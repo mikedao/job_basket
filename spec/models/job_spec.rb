b460f6031f8d9e09692a537e341af8391f390f87
Michael Dao <michael.dao@gmail.com>
2015-04-22 07:35:26 -0600

    Added basic job tests

--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -1,5 +1,33 @@
 require "rails_helper"
 
 RSpec.describe Job, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a position" do
+    job = create(:job)
+
+    expect(job.position).to eq("Janitor")
+  end
+
+  it "has a posting date" do
+    job = create(:job)
+
+    expect(job.posting_date.class).to eq(Date)
+  end
+
+  it "has a description" do
+    job = create(:job)
+
+    expect(job.description.class).to eq(String)
+  end
+
+  it "has a location" do
+    job = create(:job)
+
+    expect(job.description).to eq("Clean stuff")
+  end
+
+  it "has a location" do
+    job = create(:job)
+
+    expect(job.location).to eq("Denver, CO")
+  end
 end
