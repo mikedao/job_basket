11caca6f67283b21da8702401c43bb871583daeb
Adam Smith <adamsmith910@aol.com>
2015-04-22 03:19:12 -0600

    created routes to consume the github jobs api

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  
+  namespace :api do
+    namespace :v1 do
+      namespace :github do
+        get "/", to: "github#index"
+        get "/data", to: "data#index"
+      end
+    end
+  end
 end
