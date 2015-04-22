914999cc8a0a0e4624a396aff0d622c58f744799
Michael Dao <michael.dao@gmail.com>
2015-04-22 07:36:52 -0600

    Fixed hound issues

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :job do
-   position "Janitor"
-   posting_date Time.now
-   description "Clean stuff"
-   location "Denver, CO"
+    position "Janitor"
+    posting_date Time.now
+    description "Clean stuff"
+    location "Denver, CO"
   end
 end
