ac778ea1cc1c0e73492d006872c557eec550bb26
Nathan Owsiany <nowsiany@gmail.com>
2015-04-22 13:39:08 -0600

    create jobs POST endpoint

---
+++ app/controllers/api/jobs_controller.rb
@@ -0,0 +1,19 @@
+class Api::JobsController < ApplicationController
+  def create
+    job = Job.where(job_params).first_or_initialize
+    if !job.new_record?
+      render json: job, status: :found
+    elsif job.new_record? && job.save
+      render json: job, status: :created
+    else
+      render json: job.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def job_params
+    params.require(:job)
+      .permit(:position, :description, :posting_date, :source, :location)
+  end
+end
---
+++ spec/controllers/api/jobs_controller_spec.rb
@@ -0,0 +1,69 @@
+require 'rails_helper'
+
+RSpec.describe Api::JobsController, type: :controller do
+  describe "POST#create" do
+    it "can create a new job with no company or date" do
+      job = {
+        position: "PHP Dev",
+        description: "Only the best...",
+        source: "http://www.the-internet.com",
+        location: "London, UK"
+      }
+      post :create, job: job
+
+      new_job = JSON.parse(response.body)
+
+      expect(response.status).to eq(201)
+      expect(new_job["position"]).to eq("PHP Dev")
+      expect(new_job["posting_date"]).to eq(Date.today.strftime("%F"))
+    end
+
+    it "can create a new job with a date and no company" do
+      date = 5.days.ago
+      job = {
+        position: "PHP Dev",
+        description: "Only the best...",
+        source: "http://www.the-internet.com",
+        location: "London, UK",
+        posting_date: date
+      }
+      post :create, job: job
+
+      new_job = JSON.parse(response.body)
+
+      expect(response.status).to eq(201)
+      expect(new_job["position"]).to eq("PHP Dev")
+      expect(new_job["posting_date"]).to eq(date.strftime("%F"))
+    end
+
+    it "returns a 302 status code if existing record" do
+      create(:job, position: "PHP Dev",
+                   description: "Only the best...",
+                   source: "http://www.the-internet.com",
+                   location: "London, UK")
+
+      dup_job = {
+        position: "PHP Dev",
+        description: "Only the best...",
+        source: "http://www.the-internet.com",
+        location: "London, UK"
+      }
+      post :create, job: dup_job
+
+      new_job = JSON.parse(response.body)
+
+      expect(response.status).to eq(302)
+      expect(new_job["position"]).to eq("PHP Dev")
+    end
+
+    it "returns a 422 if invalid job content" do
+      job = {
+        position: "PHP Dev",
+        location: "London, UK"
+      }
+      post :create, job: job
+
+      expect(response.status).to eq(422)
+    end
+  end
+end
