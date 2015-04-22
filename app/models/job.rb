030c0e0d14421ebcaa3529e54723889e65230bdc
Nathan Owsiany <nowsiany@gmail.com>
2015-04-21 18:30:16 -0600

    setup basic job and company validations and relationship

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,5 @@
 class Job < ActiveRecord::Base
   belongs_to :company
+
+  validates :position, :posting_date, :description, :company_id, presence: true
 end
--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -1,5 +1,11 @@
 require "rails_helper"
 
 RSpec.describe Job, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it { should belong_to(:company)  }
+    it { should validate_presence_of(:position) }
+    it { should validate_presence_of(:posting_date) }
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:company_id) }
+  end
 end
