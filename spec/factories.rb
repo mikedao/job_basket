440d2deb10d47ba63fd5caec548c03d7964019ac
Brandon Mrsny <bmrsny@gmail.com>
2015-04-22 22:54:09 -0600

    added the user to the factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -15,4 +15,12 @@ FactoryGirl.define do
     location "Denver, CO"
     url "http://phprules.com"
   end
+
+  factory :user do
+    username "Jimbo"
+    email "Jimbo@example.com"
+    token "somelongtoken"
+    uid "1223456"
+    profile_image_url "http://turing.io/pic"
+  end
 end
