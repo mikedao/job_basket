cea301720c4c34830c155999bae525f5fc462268
Emily Berkeley <eberkeley@gmail.com>
2015-04-23 13:38:15 -0600

    Add tag validations

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,9 @@
 require "rails_helper"
 
 RSpec.describe Tag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it { should validate_presence_of(:name) }
+    it { should validate_length_of(:name).is_at_most(20) }
+    it { should have_many(:jobs) }
+  end
 end
