42237744f79d1962cdecebaee2cf3b3f8ac24401
Emily Berkeley <eberkeley@gmail.com>
2015-04-23 13:43:40 -0600

    Add job tag relationships

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -6,4 +6,12 @@ RSpec.describe Tag, type: :model do
     it { should validate_length_of(:name).is_at_most(20) }
     it { should have_many(:jobs) }
   end
+
+  describe "attributes" do
+    it "should have a name" do
+      tag = create(:tag)
+
+      expect(tag.name).to eq("Ruby")
+    end
+  end
 end
