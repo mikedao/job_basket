a3682f72e15536f5eb92f23a4c5985d70f335bb7
Michael Dao <michael.dao@gmail.com>
2015-04-23 14:14:33 -0600

    Got company method working

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,27 @@
 class Company < ActiveRecord::Base
+  after_create :get_info
+
   has_many :jobs
 
   validates :name, presence: true
+  private
+
+  def get_info
+    result = GlassdoorService.new.company(self.name)
+    self.update_attributes(url: result["website"],
+                           industry: result["industry"],
+                           logo_url: result["squareLogo"],
+                           overall_rating: result["overallRating"].to_f,
+                           rating_description: result["ratingDescription"],
+                           cultureandvalues_rating: result["cultureAndValuesRating"].to_f,
+                           seniorleadership_rating: result["seniorLeadershipRating"].to_f,
+                           compensationandbenefits_rating: result["compensationAndBenefitsRating"].to_f,
+                           careeropportunities_rating: result["careerOpportunitiesRating"].to_f,
+                           worklifebalance_rating: result["workLifeBalanceRating"].to_f,
+                           recommendtofriends_rating: result["recommendToFriendRating"].to_f,
+                           ceo_name: result["ceo"]["name"],
+                           ceo_picture: result["ceo"]["image"]["src"],
+                           approval_rating: result["ceo"]["pctApprove"].to_i
+                          )
+  end
 end
