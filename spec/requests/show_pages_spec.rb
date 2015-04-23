074e8a0ea3cf47cbb23671d43d7c6b088ca766a0
Brandon Mrsny <bmrsny@gmail.com>
2015-04-22 22:55:48 -0600

    removed unnecessary puts statement

--- spec/requests/show_pages_spec.rb
+++ spec/requests/show_pages_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe "ShowPages", type: :request do
     end
 
     it "has a single job with associated info" do
-      puts job = create(:job)
+      job = create(:job)
       visit job_path(job)
       expect(page).to have_content(job.position)
       #expect(page).to have_content("Wed, 22 Apr 2015")
