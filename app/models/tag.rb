89f55dc1b3a983c9245205c0ac0bdb8a9c4b1bb8
Emily Berkeley <eberkeley@gmail.com>
2015-04-23 13:48:54 -0600

    Add tag uniqueness validation

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,5 +2,5 @@ class Tag < ActiveRecord::Base
   has_many :jobtags
   has_many :jobs, through: :jobtags
 
-  validates :name, presence: true, length: { maximum: 20 }
+  validates :name, presence: true, length: { maximum: 20 }, uniqueness: true
 end
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Tag, type: :model do
   describe "validations" do
     it { should validate_presence_of(:name) }
     it { should validate_length_of(:name).is_at_most(20) }
+    it { should validate_uniqueness_of(:name) }
     it { should have_many(:jobs) }
   end
 
