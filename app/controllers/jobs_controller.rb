e8725de975a9ff303fc68c9cc8f5bb45f017bc07
Konr Larson <larsonkonr@gmail.com>
2015-04-24 10:59:54 -0600

    removed commented out text

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -6,9 +6,5 @@ class JobsController < ApplicationController
   def show
     @job = Job.find(params[:id])
     @job_description = @job.description.gsub("]" , "").gsub("[" , "").gsub("\"" , "").gsub("," , "")
-    # @b = @a.gsub "[" , ""
-    # @c = @b.gsub "\"" , ""
-    # @job_description = @c.gsub "," , ""
-
   end
 end
