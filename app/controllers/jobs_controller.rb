26cfadd93f2f6de0be530c3298a4510ae9c28ffd
Konr Larson <larsonkonr@gmail.com>
2015-04-24 10:38:37 -0600

    modified jobs show page, moved partials to folder

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -5,5 +5,10 @@ class JobsController < ApplicationController
 
   def show
     @job = Job.find(params[:id])
+    @a = @job.description.gsub "]" , ""
+    @b = @a.gsub "[" , ""
+    @c = @b.gsub "\"" , ""
+    @job_description = @c.gsub "," , ""
+
   end
 end
