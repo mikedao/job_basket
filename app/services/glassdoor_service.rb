25d4bf5a25aa30094e651e22dc2277d56a914d15
Emily Berkeley <eberkeley@gmail.com>
2015-04-28 17:34:52 -0600

    Pull out URL params for readability

--- app/services/glassdoor_service.rb
+++ app/services/glassdoor_service.rb
@@ -6,7 +6,7 @@ class GlassdoorService
   end
 
   def company(company_name)
-    parse(@connection.get("api.htm?t.p=#{Figaro.env.glassdoor_partner_id}&t.k=#{Figaro.env.glassdoor_key}&userip=0.0.0.0&useragent=&format=json&v=1&action=employers&q=#{company_name}"))
+    parse(@connection.get(url_params(company_name)))
   end
 
   private
@@ -22,5 +22,16 @@ class GlassdoorService
     employers.select do |employer|
       employer["exactMatch"] == true
     end
+  end
+
+  def url_params(company_name)
+    ("api.htm?t.p=#{Figaro.env.glassdoor_partner_id}&
+     t.k=#{Figaro.env.glassdoor_key}&
+     userip=0.0.0.0&
+     useragent=&
+     format=json&
+     v=1&
+     action=employers&
+     q=#{company_name}").gsub(/\s/, "")
   end
 end
