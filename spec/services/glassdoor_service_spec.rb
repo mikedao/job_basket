fd01ea65ec8ccdaa333087d7537826b4c43f22d6
Emily Berkeley <eberkeley@gmail.com>
2015-04-28 08:24:01 -0600

    Revert "Update Glassdoor Service to only return the first exact match"

--- spec/services/glassdoor_service_spec.rb
+++ spec/services/glassdoor_service_spec.rb
@@ -1,23 +1,16 @@
 require "rails_helper"
 
 RSpec.describe GlassdoorService do
-  let(:service) { GlassdoorService.new }
-
   it "has a connection" do
+    service = GlassdoorService.new
+
     expect(service.class).to eq(GlassdoorService)
   end
 
   it "hits glassdoor for company information" do
+    service = GlassdoorService.new
+
     result = service.company("Twitter")
     expect(result["ceo"]["name"].class).to eq(String)
-  end
-
-  it "returns only exact matches" do
-    result1 = service.company("My Company")
-    result2 = service.company("My Therapy Company")
-
-    expect(result1).to be_nil
-    expect(result2.class).to eq(Hash)
-    expect(result2["website"]).to eq("www.mytherapycompany.com")
   end
 end
