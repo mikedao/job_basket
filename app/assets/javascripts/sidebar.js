4cf37f273c48aaaeccc0ec68d5ef3ef7ff1a160c
Konr Larson <larsonkonr@gmail.com>
2015-04-30 14:47:51 -0600

    changed from single to double quotes

--- app/assets/javascripts/sidebar.js
+++ app/assets/javascripts/sidebar.js
@@ -3,9 +3,9 @@ $(document).ready(function() {
     var tags = ["hello"];
     var bar = $("#search-bar");
     bar.keyup(function(){
-      $('.search-item').hide().filter(function() {
+      $(".search-item").hide().filter(function() {
         var itemText = $(this).text().toLowerCase();
-        var searchText = $('#search-bar').val().toLowerCase();
+        var searchText = $("#search-bar").val().toLowerCase();
         tags.push(searchText);
         var filterMatch = itemText.indexOf(searchText) != -1;
         return filterMatch;
@@ -14,22 +14,19 @@ $(document).ready(function() {
   }
 
   function initializeTags() {
-    $('div.tags').find('input:checkbox').on('click', function () {
-      $('.jobs-table > li').hide();
-      if( $('div.tags').find('input:checked').length > 0)
+    $("div.tags").find("input:checkbox").on("click", function () {
+      $(".jobs-table > li").hide();
+      if( $("div.tags").find("input:checked").length > 0)
     {
-      $('div.tags').find('input:checked').each(function () {
-        $('.jobs-table > li.' + $(this).attr('rel')).show();
+      $("div.tags").find("input:checked").each(function () {
+        $(".jobs-table > li." + $(this).attr("rel")).show();
       });
     }else{
-      $('.jobs-table > li').show();
+      $(".jobs-table > li").show();
     }
     });
   }
 
   initializeSearchBar();
-  console.log("Search Bar ready");
-
   initializeTags();
-  console.log("Tags ready");
 });
