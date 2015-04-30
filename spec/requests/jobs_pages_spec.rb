67398c8cb28c608b1939caaa93007efdfe136bf8
Emily Berkeley <eberkeley@gmail.com>
2015-04-30 15:10:13 -0600

    Update job pages spec

---
+++ spec/requests/jobs_pages_spec.rb
@@ -0,0 +1,44 @@
+require "rails_helper"
+
+RSpec.describe "JobsPages", type: :request do
+  before do
+    @user = create(:user)
+    allow_any_instance_of(ApplicationController)
+      .to receive(:current_user).and_return(@user)
+  end
+
+  describe "GET /jobs" do
+    it "can like jobs" do
+      job = create(:job)
+      @user.jobsliked << job
+
+      visit jobs_path
+
+      expect(page).to have_content(job.position)
+      expect(page).to have_content(job.location)
+      expect(page).to have_content("Added to My Jobs")
+    end
+
+    it "can hide jobs" do
+      hidden_job = create(:job, position: "Hiding job")
+      @user.jobshidden << hidden_job
+
+      visit jobs_path
+
+      expect(page).not_to have_content("Hiding job")
+    end
+  end
+
+  describe "GET /job/:id" do
+    it "has a single job with associated info" do
+      job = create(:job)
+
+      visit job_path(job)
+
+      expect(page).to have_content(job.position)
+      expect(page).to have_content(Date.today.strftime("%B %e, %Y"))
+      expect(page).to have_content(job.description)
+      expect(page).to have_content(job.location)
+    end
+  end
+end
