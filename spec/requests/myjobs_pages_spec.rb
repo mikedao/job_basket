31c0a6a8c5fba52b7b676bb53287e0f205abe854
Emily Berkeley <eberkeley@gmail.com>
2015-04-30 15:15:41 -0600

    Add assertions to ensure that hidden and not liked jobs don't appear on my_jobs

--- spec/requests/myjobs_pages_spec.rb
+++ spec/requests/myjobs_pages_spec.rb
@@ -1,23 +1,28 @@
 require "rails_helper"
 
 RSpec.describe "MyjobsPages", type: :request do
-  describe "GET /myjobs_pages" do
+  describe "GET /myjobs" do
     before do
       @user = create(:user)
       allow_any_instance_of(ApplicationController)
       .to receive(:current_user).and_return(@user)
     end
 
-    it "has liked jobs" do
+    it "has only liked jobs" do
       job = create(:job)
-      hidden_job = create(:job)
+      create(:job, position: "Not liked yet")
+      hidden_job = create(:job, position: "Hidden job", location: "nowhere")
       @user.jobsliked << job
       @user.jobshidden << hidden_job
+
       visit my_jobs_path
 
       expect(page).to have_content(job.position)
       expect(page).to have_content(job.location)
       expect(page).to have_content("Added to My Jobs")
+      expect(page).to_not have_content("Not liked yet")
+      expect(page).to_not have_content("Hidden job")
+      expect(page).to_not have_content("nowhere")
     end
   end
 end
