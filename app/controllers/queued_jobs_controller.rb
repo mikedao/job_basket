17b5ec684eb677ae5df50185d1b2b78e470bbd1f
Scott Crawford <scottcrawford03@gmail.com>
2015-05-03 11:56:44 -0600

    working the queue

--- app/controllers/queued_jobs_controller.rb
+++ app/controllers/queued_jobs_controller.rb
@@ -6,4 +6,14 @@ class QueuedJobsController < ApplicationController
   def edit
     @job = Job.find(params[:id])
   end
+
+  def update
+    job = Job.find(params[:id])
+    job.update(
+      approved: true,
+      position: params[:job][:position],
+      description: params[:job][:description],
+      location: params[:job][:location])
+    redirect_to queued_jobs_path
+  end
 end
