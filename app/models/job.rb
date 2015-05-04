c32ba694fd6c40497d654d37053e8bebc434c55d
Emily Berkeley <eberkeley@gmail.com>
2015-05-04 14:24:40 -0600

    Refactor Job posting_date before_validation

--- app/models/job.rb
+++ app/models/job.rb
@@ -8,7 +8,7 @@ class Job < ActiveRecord::Base
 
   validates :position, :description, :source, presence: true
 
-  before_validation { |job| job.posting_date = Date.today if !job.posting_date }
+  before_validation { self.posting_date ||= Date.today }
 
   scope :by_newest, -> { order("posting_date DESC") }
   default_scope { by_newest }
